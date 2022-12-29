abf678b1f298a69ab5d414805196b5fc31d197d5
fathyafathazz <fathyafathazzra@gmail.com>
2022-12-29 11:57:28 +0700

    praktikum 9

---
+++ upload/form_upload.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Upload</title>
+</head>
+<body>
+    <h2> Penggunaan Upload </h2>
+    <pre>
+    <form action="proses_upload.php" method="post" enctype="multipart/form-data">
+    File : <input type="file" name="file1"> <input type="submit"
+    name="upload" value="Upload">
+    </form>
+    </pre>
+</body>
+</html>
